ac01e73fd05f320382420c268f1b0fa4c53ba19f
Rafael <proyectorafael123@gmail.com>
2021-05-11 14:22:55 -0600

    rama rafael con tabla tipo avion

---
+++ src/main/java/com/airwings/app/model/entity/TipoAvion.java
@@ -0,0 +1,23 @@
+
+package com.airwings.app.model.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="tipo_avion")
+public class TipoAvion implements Serializable{
+    private static final long serialVersionUID=1L;
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty
+    private String nombre;
+}
